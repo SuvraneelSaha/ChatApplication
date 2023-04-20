5e8d68c2e8e2ff9c4703bd035b11c684d52042d9
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-04-20 22:34:03 +0530

    api key added and also the private key

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyBqp2j-KwUetTOJfbVq2e3WJRAMNozF1lQ",
+  authDomain: "chat-e4e72.firebaseapp.com",
+  projectId: "chat-e4e72",
+  storageBucket: "chat-e4e72.appspot.com",
+  messagingSenderId: "16111954181",
+  appId: "1:16111954181:web:697a0936a05f60a8d22f6c",
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
