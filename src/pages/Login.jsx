0bfcfd0f868558c6c463a9cd7f60ff56cf6090d7
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-04-28 13:38:24 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Furry Friends</span>
-        <span className="title"> LogIn </span>
+        <span className="title">*Log In with your Furry Friends username*</span>
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
